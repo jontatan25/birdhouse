4ae773e1d94e7481f0178176f5a80798cdbe301b
Jonathan Sarmiento <jhonn.photo@gmail.com>
2023-03-18 06:58:40 -0500

    feat/endpoint 'POST/house' created

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BirdhouseController } from './birdhouse/birdhouse.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, BirdhouseController],
   providers: [AppService],
 })
 export class AppModule {}
